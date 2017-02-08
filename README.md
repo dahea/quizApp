1bddb49a5328bb72fcd42564bed11cbb9598b31d
dahea <dahea.an@gmail.com>
2017-02-07 23:37:52 -0500

    updates to readme and adding form validation

--- README.md
+++ README.md
@@ -3,5 +3,18 @@ quiz app based on "how to learn js properly" online study guide
 
 to run:
 
-npm install -g localhost  
-localhost ./ 8080
+npm install -g serve  
+serve
+
+###specs to add
+
+set 1:
+- Add client-side data validation: make sure the user answers each question before proceeding to the next question.
+- Add a “Back” button to allow the user to go back and change her answer. The user can go back up to the first question. For the questions that the user has answered already, be sure to show the radio button selected, so that the user is not forced to answer the questions again, which she has completed.
+- Store the quiz questions in an external JSON file.
+- Add user authentication: allow users log in, and save their login credentials to local storage (HTML5 browser storage).
+- Use cookies to remember the user, and show a “Welcome, First Name” message when the user returns to the quiz.
+
+set 2:
+- Learn Handlebars.js and add Handlebars.js templating to the quiz. You should no longer have any HTML in your JavaScript code. Your quiz is getting more advanced bit by bit.
+- Keep a record of all the users who take the quiz and show each user how her score ranks amongst the scores from other quiz takers.
--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,81 @@
 window.onload=function(){
-// Only one question is in this array, but you will add all the questions
-var allQuestions = [
-	{question: "Who is Prime Minister of the United Kingdom?",
-  choices: ["David Cameron1", "Gordon Brown", "Winston Churchill", "Tony Blair"],
-  correctAnswer:0},
-  {question: "Who are you?",
-  choices: ["David Cameron2", "Gordon Brown", "Winston Churchill", "Tony Blair"],
-  correctAnswer:1},
-  {question: "Who am I?",
-  choices: ["David Cameron3", "Gordon Brown", "Winston Churchill", "Tony Blair"],
-  correctAnswer:2},
-];
 
-var questionCounter = allQuestions.length;
-var correctAnswerCounter = 0;
-var currentQuestion = 0;
-var isLastQuestion = false;
-var nextButton = document.getElementById('next');
-var choicesWrapper = document.getElementById('choices');
-var questionWrapper = document.getElementById('question');
+  // Only one question is in this array, but you will add all the questions
+  var allQuestions = [
+  	{question: "Who is Prime Minister of the United Kingdom?",
+    choices: ["David Cameron1", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+    correctAnswer:0},
+    {question: "Who are you?",
+    choices: ["David Cameron2", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+    correctAnswer:1},
+    {question: "Who am I?",
+    choices: ["David Cameron3", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+    correctAnswer:2},
+  ];
 
-function clearNode(node){
-	while (node.hasChildNodes()) {
-    node.removeChild(node.lastChild);
-	}
-}
+  var questionCounter = allQuestions.length;
+  var correctAnswerCounter = 0;
+  var currentQuestion = 0;
+  var isLastQuestion = false;
+  var nextButton = document.getElementById('next');
+  var choicesWrapper = document.getElementById('choices');
+  var questionWrapper = document.getElementById('question');
+  var msgWrapper = document.getElementById('msg');
 
-function displayQuestion(questionNum){
-  questionWrapper.innerHTML = allQuestions[questionNum].question;   
-  clearNode(choicesWrapper);
-
-for(var i=0; i < allQuestions[questionNum].choices.length; i++) {
-  	var inputnode = document.createElement("input");
-    inputnode.setAttribute("type","radio");
-    inputnode.setAttribute("name","answer");
-    inputnode.setAttribute("value",i);
-    var brnode = document.createElement("br");
-  	var textnode = document.createTextNode(allQuestions[questionNum].choices[i].toString());
-  	choicesWrapper.appendChild(inputnode);
-    choicesWrapper.appendChild(textnode);
-    choicesWrapper.appendChild(brnode);
-  }  
-}
-
-function checkAnswer(questionNum){
-	var correctAnswerValue = Number(allQuestions[questionNum].correctAnswer);
-  var selectedAnswerValue = Number(choicesWrapper.querySelector('input[name="answer"]:checked').value);
-	if(correctAnswerValue === selectedAnswerValue){
-  	correctAnswerCounter++;
+  function clearNode(node){
+  	while (node.hasChildNodes()) {
+      node.removeChild(node.lastChild);
+  	}
   }
-}
 
-function displayScore(score){
-	clearNode(choicesWrapper);
-  clearNode(questionWrapper);
-  questionWrapper.innerHTML = "Your score: "+score;    
-}
+  function displayQuestion(questionNum){
+    questionWrapper.innerHTML = allQuestions[questionNum].question;  
+    clearNode(choicesWrapper);
 
-nextButton.onclick = function(){
-  checkAnswer(currentQuestion);
-  if(currentQuestion < (questionCounter-1)){
-    currentQuestion++;
-    displayQuestion(currentQuestion);
-  } else {
-    isLastQuestion = true;
-    displayScore(correctAnswerCounter);
+    for(var i=0; i < allQuestions[questionNum].choices.length; i++){
+      var inputnode = document.createElement("input");
+      inputnode.setAttribute("type","radio");
+      inputnode.setAttribute("name","answer");
+      inputnode.setAttribute("value",i);
+      var brnode = document.createElement("br");
+      var textnode = document.createTextNode(allQuestions[questionNum].choices[i].toString());
+      choicesWrapper.appendChild(inputnode);
+      choicesWrapper.appendChild(textnode);
+      choicesWrapper.appendChild(brnode);
+    }  
   }
-};
 
-displayQuestion(currentQuestion);
+  function checkAnswer(questionNum){
+    var correctAnswerValue = Number(allQuestions[questionNum].correctAnswer);
+    var selectedAnswerValue = Number(choicesWrapper.querySelector('input[name="answer"]:checked').value);
+    if(correctAnswerValue === selectedAnswerValue){
+     correctAnswerCounter++;
+    }
+  }
 
+  function displayScore(score){
+  	clearNode(choicesWrapper);
+    clearNode(questionWrapper);
+    msgWrapper.innerHTML = "Your score: "+score;    
+  }
 
+  nextButton.onclick = function(){
 
+    if(choicesWrapper.querySelector('input[name="answer"]:checked')){
+      clearNode(msgWrapper);
+      checkAnswer(currentQuestion);
+      if(currentQuestion < (questionCounter-1)){
+        currentQuestion++;
+        displayQuestion(currentQuestion);
+      } else {
+        isLastQuestion = true;
+        displayScore(correctAnswerCounter);
+      }
+    } else {
+      msgWrapper.innerHTML = "Please choose an option";
+    }
+  };
+
+  displayQuestion(currentQuestion);
 
 }
